8c0fc0a5537e3ea68b94370f528437ea4abf3760
ajuijas <ijas.ahmd.ap@gmail.com>
2025-02-17 14:21:24 +0530

    fixed a test

--- redis/cmd/root_test.go
+++ redis/cmd/root_test.go
@@ -240,7 +240,7 @@ func Test_atominc_multi_ops(t *testing.T) {
 	value := sendDBCommand("get key", conn3)
 
 	if value != "(nil)\n" && value != "10000" && value != "20000"{
-		// The test is failed if the value is not 10000 or 20000 or (nil), since the operations are atomic
+		// The test is failed when the 'got value' is not 10000 or 20000 or (nil), since the operations are atomic
 		t.Errorf("Expected <<%v>> Got <<%v>>", "any of (10000, 20000, (nil))", value)
 	}
 }
